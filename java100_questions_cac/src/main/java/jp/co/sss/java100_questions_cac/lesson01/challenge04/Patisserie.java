b9e1b82d68d1ccdd10c361532fe4a79351f10119
西村和眞（NISHIMURAKazuma <NISHIMURAKazuma@192.168.0.86>
2026-04-14 17:15:40 +0900

    challenge04

--- java100_questions_cac/src/main/java/jp/co/sss/java100_questions_cac/lesson01/challenge04/Patisserie.java
+++ java100_questions_cac/src/main/java/jp/co/sss/java100_questions_cac/lesson01/challenge04/Patisserie.java
@@ -11,7 +11,7 @@
  *
  *
  * <実行例>
- *
+ 
  * それぞれ何個ずつ買いますか？（最大30個まで）
  *
  * シトロン      >8
@@ -28,6 +28,39 @@
 
 package jp.co.sss.java100_questions_cac.lesson01.challenge04;
 
-public class Patisserie {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		int num1 = 30;
+		int num2 = 30;
+		int num3 = 30;
+		System.out.println("\n本日のおすすめ商品です。");
+		System.out.println("シトロン  　\\250・・・ 残り" + num1 + "個");
+		System.out.println("ショコラ　　\\280・・・ 残り" + num2 + "個");
+		System.out.println("ピスタージュ　　 \\320・・・ 残り" + num3 + "個");
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大３０個まで）");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン＞");
+		String str = reader.readLine();
+		int num01 = Integer.parseInt(str);
+
+		System.out.print("ショコラ＞");
+		String str1 = reader.readLine();
+		int num02 = Integer.parseInt(str1);
+
+		System.out.print("ピスターシュ＞");
+		String str2 = reader.readLine();
+		int num03 = Integer.parseInt(str2);
+
+		System.out.println("\n シトロン" + num01 + "個");
+		System.out.println("\n ショコラ" + num02 + "個");
+		System.out.println("\n ピスタージュ" + num03 + "個");
+		System.out.println("\nをお買い上げですね。\n承りました。");
+
+	}
 }
